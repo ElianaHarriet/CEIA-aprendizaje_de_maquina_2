61ac452d7bfc86e1cca12003a6176d9580898ba9
Pablo Santiago Rodriguez Castro <pablosantiagorodriguezcastro@Pablos-MacBook-Pro.local>
2026-05-31 19:22:46 -0300

    fix: cambio de llamado a yamls de connections y variables por jsons en bootstrap

--- dockerfiles/airflow/bootstrap.sh
+++ dockerfiles/airflow/bootstrap.sh
@@ -3,12 +3,12 @@ set -e
 
 # Si el comando es un servicio principal de Airflow, importar variables/conexiones al metadata DB y luego arrancar el servicio
 case "$1" in
-  api-server|scheduler|worker|triggerer)
-    if [ -f /opt/airflow/secrets/variables.yaml ]; then
-      airflow variables import /opt/airflow/secrets/variables.yaml || true
+  api-server|scheduler|worker|triggerer|dag-processor)
+    if [ -f /opt/airflow/secrets/variables.json ]; then
+      airflow variables import /opt/airflow/secrets/variables.json || true
     fi
-    if [ -f /opt/airflow/secrets/connections.yaml ]; then
-      airflow connections import /opt/airflow/secrets/connections.yaml || true
+    if [ -f /opt/airflow/secrets/connections.json ]; then
+      airflow connections import /opt/airflow/secrets/connections.json || true
     fi
     exec /entrypoint "$@"
     ;;
