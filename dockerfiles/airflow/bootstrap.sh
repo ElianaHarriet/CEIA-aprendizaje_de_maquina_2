c8e6ae9c65ce46be36868813255c457e333ce790
Pablo Santiago Rodriguez Castro <pablosantiagorodriguezcastro@Pablos-MacBook-Pro.local>
2026-05-31 21:16:09 -0300

    fix: fixed bootstrap variables path

--- dockerfiles/airflow/bootstrap.sh
+++ dockerfiles/airflow/bootstrap.sh
@@ -4,11 +4,11 @@ set -e
 # Si el comando es un servicio principal de Airflow, importar variables/conexiones al metadata DB y luego arrancar el servicio
 case "$1" in
   api-server|scheduler|worker|triggerer|dag-processor)
-    if [ -f /opt/airflow/secrets/variables.json ]; then
-      airflow variables import /opt/airflow/secrets/variables.json || true
+    if [ -f /opt/secrets/variables.json ]; then
+      airflow variables import /opt/secrets/variables.json || true
     fi
-    if [ -f /opt/airflow/secrets/connections.json ]; then
-      airflow connections import /opt/airflow/secrets/connections.json || true
+    if [ -f /opt/secrets/connections.json ]; then
+      airflow connections import /opt/secrets/connections.json || true
     fi
     exec /entrypoint "$@"
     ;;
